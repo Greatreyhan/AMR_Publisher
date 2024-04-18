fecf9af03b688e2b08acf11934dcf4118496e4bd
Greatreyhan <greatreyhan@students.undip.ac.id>
2024-04-18 14:03:46 +0700

    push to mqtt broker

---
+++ data_parser.py
@@ -0,0 +1,147 @@
+
+# Function to calculate checksum
+def checksum_pc_generator(data):
+    checksum = 0
+    for byte in data:
+        checksum += byte
+    return checksum & 0xFF
+
+# Function to parse BNO08X data packet
+def parse_BNO08X_packet(packet):
+    if len(packet) != 16:
+        print('Not long enough')
+        return None  # Packet length is not correct
+    if packet[0] != 0xA5 or packet[1] != 0x5A:
+        print('incorrect header')
+        return None  # Header bytes are not correct
+    if packet[15] != checksum_pc_generator(packet[:15]):
+        print('checksum wrong')
+        return None  # Checksum doesn't match
+    
+    BNO08x = {
+        'yaw': (packet[3] << 8) | packet[4],
+        'pitch': (packet[5] << 8) | packet[6],
+        'roll': (packet[7] << 8) | packet[8],
+        'x_acceleration': (packet[9] << 8) | packet[10],
+        'y_acceleration': (packet[11] << 8) | packet[12],
+        'z_acceleration': (packet[13] << 8) | packet[14]
+    }
+
+    print(BNO08x)
+    
+    return BNO08x
+
+# Function to parse Sensor data packet
+def parse_Sensor_packet(packet):
+    if len(packet) != 16:
+        print('Not long enough')
+        return None  # Packet length is not correct
+    if packet[0] != 0xA5 or packet[1] != 0x5A:
+        print('incorrect header')
+        return None  # Header bytes are not correct
+    if packet[15] != checksum_pc_generator(packet[:15]):
+        print('checksum wrong')
+        return None  # Checksum doesn't match
+    
+    Sensor = {
+        'temperature': (packet[3] << 8) | packet[4],
+        'humidity': (packet[5] << 8) | packet[6],
+        'current': (packet[7] << 8) | packet[8],
+        'voltage': (packet[9] << 8) | packet[10],
+        'loadcell': (packet[11] << 8) | packet[12]
+    }
+
+    print(Sensor)
+    
+    return Sensor
+
+# Function to parse Ping
+def parse_pc_ping_response_packet(packet):
+    if len(packet) != 16:
+        print('Not long enough')
+        return None  # Packet length is not correct
+    if packet[0] != 0xA5 or packet[1] != 0x5A:
+        print('incorrect header')
+        return None  # Header bytes are not correct
+    if packet[15] != checksum_pc_generator(packet[:15]):
+        print('checksum wrong')
+        return None  # Checksum doesn't match
+    
+    print("Ping")
+
+    return True if packet[15] == 0 else False
+
+# Function to parse Encoder
+def parse_Encoder_Package_packet(packet):
+    if len(packet) != 16:
+        print('Not long enough')
+        return None  # Packet length is not correct
+    if packet[0] != 0xA5 or packet[1] != 0x5A:
+        print('incorrect header')
+        return None  # Header bytes are not correct
+    if packet[15] != checksum_pc_generator(packet[:15]):
+        print('checksum wrong')
+        return None  # Checksum doesn't match
+    
+    Encoder_Package = {
+        'vertical_distance': (packet[3] << 8) | packet[4],
+        'horizontal_distance': (packet[5] << 8) | packet[6],
+        'vertical_speed': (packet[7] << 8) | packet[8],
+        'horizontal_speed': (packet[9] << 8) | packet[10]
+    }
+
+    print(Encoder_Package)
+    
+    return Encoder_Package
+
+# Function to parse Kinematic
+def parse_Kinematic_packet(packet):
+    if len(packet) != 16:
+        print('Not long enough')
+        return None  # Packet length is not correct
+    if packet[0] != 0xA5 or packet[1] != 0x5A:
+        print('Incorrect header')
+        return None  # Header bytes are not correct
+    if packet[15] != checksum_pc_generator(packet[:15]):
+        print('Checksum wrong')
+        return None  # Checksum doesn't match
+    
+    Sx = (packet[3] << 8) | packet[4]
+    Sy = (packet[5] << 8) | packet[6]
+    St = (packet[7] << 8) | packet[8]
+    T = (packet[9] << 8) | packet[10]
+    
+    Kinematic_data = {
+        'Sx': Sx,
+        'Sy': Sy,
+        'St': St,
+        'T': T
+    }
+
+    print(Kinematic_data)
+    
+    return Kinematic_data
+
+# Function to parse DWM
+def parse_DWM_packet(packet):
+    if len(packet) != 16:
+        print('Packet length is not correct')
+        return None
+    if packet[0] != 0xA5 or packet[1] != 0x5A:
+        print('Header bytes are not correct')
+        return None
+    if packet[15] != checksum_pc_generator(packet[:15]):
+        print('Checksum is wrong')
+        return None
+    
+    Xpos = (packet[3] << 8) | packet[4]
+    Ypos = (packet[5] << 8) | packet[6]
+    
+    DWM_data = {
+        'Xpos': Xpos,
+        'Ypos': Ypos
+    }
+
+    print(DWM_data)
+    
+    return DWM_data
---
+++ main.py
@@ -0,0 +1,55 @@
+import serial
+import data_parser
+import mqtt_publisher
+
+# Define the serial port and baudrate
+serial_port = 'COM19'  # or 'COM1' for Windows
+baud_rate = 115200
+
+# Create a serial object
+ser = serial.Serial(serial_port,
+                     baudrate=baud_rate,
+                     timeout=5.0,
+                     bytesize=8,
+                     parity='N',
+                     stopbits=1)
+
+mqtt_client = mqtt_publisher.connect()
+
+try:
+    while True:
+        # Read until the start of the header (0xA5)
+        header_byte1 = ser.read()
+        if header_byte1 == b'\xA5':
+            # Once the first header byte is found, check the second header byte
+            header_byte2 = ser.read()
+            if header_byte2 == b'\x5A':
+                # Reading the command
+                cmd_data = ser.read()
+                # Read the remaining 14 bytes of data
+                data = ser.read(13)
+                # Combine header bytes and data
+                packet = header_byte1 + header_byte2 + cmd_data + data
+
+                # Parsing Data
+                if(cmd_data == b'\x01'):
+                    data_parser.parse_pc_ping_response_packet(packet)
+                elif(cmd_data == b'\x02'):
+                    data_parser.parse_BNO08X_packet(packet)
+                elif(cmd_data == b'\x03'):
+                    data_parser.parse_Encoder_Package_packet(packet)
+                elif(cmd_data == b'\x04'):
+                    data_parser.parse_Sensor_packet(packet)
+                elif(cmd_data == b'\x05'):
+                    data_parser.parse_Kinematic_packet(packet)    
+                elif(cmd_data == b'\x06'):
+                    data_parser.parse_DWM_packet(packet)  
+
+                # Convert Data to String
+                parsed_data = "".join("{:02X}".format(byte) for byte in packet)
+                # Send Data to MQTT
+                mqtt_publisher.publish(mqtt_client,parsed_data) 
+
+except KeyboardInterrupt:
+    # Close the serial port when Ctrl+C is pressed
+    ser.close()
